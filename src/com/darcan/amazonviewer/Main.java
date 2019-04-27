483c3b01b5c7b1bfd5cb7dea4d320f252d78c004
Dario Candia <darcanf.94@gmail.com>
2019-04-26 21:54:02 -0400

    Main menu 85%...

--- src/com/darcan/amazonviewer/Main.java
+++ src/com/darcan/amazonviewer/Main.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
+import com.darcan.amazonviewer.models.Book;
+import com.darcan.amazonviewer.models.Chapter;
+import com.darcan.amazonviewer.models.Magazine;
 import com.darcan.amazonviewer.models.Movie;
+import com.darcan.amazonviewer.models.Serie;
 
 public class Main {
     public static void main(String[] args) {
@@ -52,7 +56,10 @@ public class Main {
             case 0:
                 System.exit(0);
             default:
-                System.out.println("Elige una opcion");
+                System.out.println();
+                System.out.println("....Selecciona una opcion!!....");
+                System.out.println();
+                exit = 1;
                 break;
             }
         } while (exit != 0);
@@ -84,13 +91,84 @@ public class Main {
         } while (exit != 0);
     }
 
+    static ArrayList<Serie> series = Serie.makeSeriesList();
+
     public static void showSeries() {
+        int exit = 1;
+        do {
+            System.out.println();
+            System.out.println("..:: SERIES ::..");
+            System.out.println();
+            series.stream().forEach(x -> System.out.println(". " + x.getTitle() + " visto: " + x.isViewed()));
+            var sc = new Scanner(System.in);
+            var response = sc.nextInt();
+            sc.close();
+            if (response == 0) {
+                exit = 0;
+                showMenu();
+            } else {
+                showChapters(series.get(response).getChapters());
+            }
+        } while (exit != 0);
     }
+
+    public static void showChapters(ArrayList<Chapter> chaptersSelected) {
+        int exit = 1;
+        do {
+            System.out.println();
+            System.out.println("..:: CHAPTERS ::..");
+            System.out.println();
+            chaptersSelected.stream().forEach(x -> System.out.println(". " + x.getTitle() + " visto: " + x.isViewed()));
+            var sc = new Scanner(System.in);
+            var response = sc.nextInt();
+            sc.close();
+            if (response == 0) {
+                exit = 0;
+                showSeries();
+            } else {
+                chaptersSelected.get(response).toSee();
+            }
+        } while (exit != 0);
+    }
+
+    static ArrayList<Book> books = Book.makeBookList();
 
     public static void showBooks() {
+        int exit = 1;
+        do {
+            System.out.println();
+            System.out.println("..:: BOOKS ::..");
+            System.out.println();
+            books.stream().forEach(x -> System.out.println(". " + x.getTitle() + " visto: " + x.isReaded()));
+            var sc = new Scanner(System.in);
+            var response = sc.nextInt();
+            sc.close();
+            if (response == 0) {
+                exit = 0;
+                showMenu();
+            } else {
+                books.get(response).toSee();
+            }
+        } while (exit != 0);
     }
 
+    static ArrayList<Magazine> magazines = Magazine.makeMagazineList();
+
     public static void showMagazines() {
+        int exit = 1;
+        do {
+            System.out.println();
+            System.out.println("..:: MAGAZINES ::..");
+            System.out.println();
+            magazines.stream().forEach(x -> System.out.println(". " + x.getTitle()));
+            var sc = new Scanner(System.in);
+            var response = sc.nextInt();
+            sc.close();
+            if (response == 0) {
+                exit = 0;
+                showMenu();
+            }
+        } while (exit != 0);
     }
 
     public static void MakeReport() {
--- src/com/darcan/amazonviewer/models/Magazine.java
+++ src/com/darcan/amazonviewer/models/Magazine.java
@@ -1,9 +1,39 @@
 package com.darcan.amazonviewer.models;
 
 import java.util.Date;
+import java.util.ArrayList;
 
 public class Magazine extends Publication {
+    private int id;
+
     public Magazine(String title, Date editionDate, String editorial) {
         super(title, editionDate, editorial);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+
+        String detailMagazine = "\n :: MAGAZINE ::" + "\n Title: " + getTitle() + "\n Editorial: " + getEditorial()
+                + "\n Edition Date: " + getEditionDate() + "\n Authors: ";
+        for (int i = 0; i < getAuthors().length; i++) {
+            detailMagazine += "\t" + getAuthors()[i] + " ";
+        }
+        return detailMagazine;
+    }
+
+    public static ArrayList<Magazine> makeMagazineList() {
+        ArrayList<Magazine> magazines = new ArrayList<>();
+        String[] authors = new String[3];
+        for (int i = 0; i < authors.length; i++) {
+            authors[i] = "author" + i;
+        }
+        for (int i = 1; i <= 5; i++) {
+            magazines.add(new Magazine("Magazine " + i, new Date(), "Editorial " + i));
+        }
+        return magazines;
     }
 }
